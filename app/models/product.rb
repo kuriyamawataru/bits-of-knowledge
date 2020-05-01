170a0b7d87f0201fc78fa6aa27c6c7b91d224dda
kuriyamawataru <watarukuriyama02182@gmail.com>
2020-05-01 20:44:10 +0900

    商品かご導入と一覧表示

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,11 @@
 class Product < ApplicationRecord
-  validates :name, presence: true
+  has_many :basket_products, dependent: :destroy
+
+
+  validates :name, presence: true 
   validates :description, presence: true
   validates :price, presence: true
   validates :unit, presence: true
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200429044554) do
+ActiveRecord::Schema.define(version: 20200430122539) do
+
+  create_table "basket_products", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "basket_id",  null: false
+    t.integer  "product_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["basket_id"], name: "index_basket_products_on_basket_id", using: :btree
+    t.index ["product_id"], name: "index_basket_products_on_product_id", using: :btree
+  end
+
+  create_table "baskets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_baskets_on_user_id", using: :btree
+  end
 
   create_table "products", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",                                      null: false
@@ -38,4 +54,7 @@ ActiveRecord::Schema.define(version: 20200429044554) do
     t.index ["name"], name: "index_users_on_name", unique: true, using: :btree
   end
 
+  add_foreign_key "basket_products", "baskets"
+  add_foreign_key "basket_products", "products"
+  add_foreign_key "baskets", "users"
 end
